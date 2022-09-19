8a1a2a3c1a9fc134040d8f7908a78882f304b856
moorebioinfo <matthew.p.moore@warwick.ac.uk>
2022-09-19 18:35:32 +0100

    Add new matching step

--- KmerAperture.py
+++ KmerAperture.py
@@ -32,13 +32,6 @@ def add_args(a):
         type=int,
         default=21,
         required=False,
-        #)
-    #parser.add_argument(
-    #"--sensitive",
-    #"-s",
-    #help="Run in sensitive mode",
-    #default=False,
-    #action="store_true"
     )
 
     args = parser.parse_args(a)
@@ -93,14 +86,12 @@ def get_indices(kmeruniq, kmers):
     return indexlist
 
 def get_accessory(kmer1ranges, ksize):
-    #SNPs =0
     acclength = 0
     allSNPranges = []
     accranges=[]
     for pair in kmer1ranges:
         rangediff = pair[1] - pair[0]
         if rangediff == ksize:
-            #SNPs+=1
             allSNPranges.append(pair)
         if (rangediff>=(ksize*2)):
             acclength+=rangediff
@@ -116,21 +107,36 @@ def assert_kmer(kmerranges, k, kmers2):
         startpos = pair[0]
         endpos = pair[1]
 
+        #Get all possible 1st and final kmers rc/non-rc
         kf=kmers2[startpos]
         ke=kmers2[endpos-1]
         kf_rc = screed.rc(kf)
         ke_rc = screed.rc(ke)
 
+        #Reconstruct middle kmer from first and final
+        #Where reconstruction matches, the series is a kmer
         kgap = int((k-1)/2)
+
         km = kmers2[startpos+kgap]
+
         kt1=(kf[kgap:] + ke[1:kgap+1])
         kt2=(kf_rc[kgap:] + ke[1:kgap+1])
         kt3=(kf[kgap:] + ke_rc[1:kgap+1])
         kt4=(kf_rc[kgap:] + ke_rc[1:kgap+1])
         kp=[kt1,kt2,kt3,kt4]
+
         if str(km) in kp:
             SNPs+=1
-    return(SNPs)
+
+            #reconstruct original sequence without middle base
+            mkmer0 = kf[0:-1] +'-'+ke[1:]
+            mkmer1 =  kf_rc[0:-1] +'-'+ke[1:]
+            mkmer2 = kf[0:-1] +'-'+ke_rc[1:]
+            mkmer3 = kf_rc[0:-1] +'-'+ke_rc[1:]
+            klist.extend([mkmer0, mkmer1, mkmer2, mkmer3])
+
+
+    return(SNPs, klist)
 
 #def find_matching(reflist, querylist):
 #    return len(set(reflist).intersection(set(querylist)))
@@ -144,7 +150,7 @@ def run_KmerAperture(gList, reference, ksize):
 
     outname = f'./{reference}_{ksize}.csv'
     output=open(outname, "w")
-    output.write('gID,Jaccard,Union,Intersection,SNP1,SNP2,SNPmean,acc1,acc2\n')
+    output.write('gID,Jaccard,Union,Intersection,SNP1,SNP2,matchedSNP,SNPmean,acc1,acc2\n')
 
     outname2 = f'./{reference}_{ksize}_timings.csv'
     output2=open(outname2, "w")
@@ -165,18 +171,19 @@ def run_KmerAperture(gList, reference, ksize):
         kmer2indices.sort()
         kmer2ranges = get_ranges(kmer2indices)
         SNPranges2, accranges2, acclength2 = get_accessory(kmer2ranges, ksize)
-        
-        kmer2SNPs = assert_kmer(SNPranges2, ksize, kmers2)
+
+        kmer2SNPs, klist2 = assert_kmer(SNPranges2, ksize, kmers2)
 
         kmer1uniq = get_uniques(kmer1set, kmer2set)
         kmer1indices = get_indices(kmer1uniq, kmers1)
         kmer1indices.sort()
         kmer1ranges = get_ranges(kmer1indices)
         SNPranges1, accranges1, acclength1 = get_accessory(kmer1ranges, ksize)
-        
-        kmer1SNPs = assert_kmer(SNPranges1, ksize, kmers1)
-        
-        matchedSNPs=int((kmer1SNPs+kmer2SNPs)/2)
+
+        kmer1SNPs, klist1 = assert_kmer(SNPranges1, ksize, kmers1)
+
+        meanSNPs=int((kmer1SNPs+kmer2SNPs)/2)
+        matchedSNPs = len(set(klist1).intersection(set(klist2)))
         analysistime = (time.time())-analysistime0
 
         intersection = len(kmer1set.intersection(kmer2set))
@@ -184,7 +191,7 @@ def run_KmerAperture(gList, reference, ksize):
         jaccard = intersection/union
         setanalysistime = (time.time())-time0
 
-        result =f"{genome2},{jaccard},{union},{intersection},{kmer1SNPs},{kmer2SNPs},{matchedSNPs},{acclength1},{acclength2}\n"
+        result =f"{genome2},{jaccard},{union},{intersection},{kmer1SNPs},{kmer2SNPs},{matchedSNPs},{meanSNPs},{acclength1},{acclength2}\n"
         output.write(result)
         print(result)
 
